b62a8f96d2b4c6eff28f920f396439afe801af54
Abhishek Sengupta <abhishek.sengupta047@gmail.com>
2020-11-23 16:10:32 +0530

    Example2 added

---
+++ src/main/scala/com/shredder/cca/Example2.scala
@@ -0,0 +1,60 @@
+package com.shredder.cca
+
+import org.apache.spark.sql.SparkSession
+
+object Example2 extends App {
+
+  val spark = SparkSession
+    .builder()
+    .appName("CCA Practice")
+    .master("local[*]")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+
+  spark.read.option("header",value = true).csv("data/orders.csv").createOrReplaceTempView("orders")
+  spark.read.option("header",value = true).csv("data/order_items.csv").createOrReplaceTempView("orders_items")
+  spark.read.option("header",value = true).csv("data/order_item_refunds.csv").createOrReplaceTempView("orders_items_refunds")
+  spark.read.option("header",value = true).csv("data/products.csv").createOrReplaceTempView("products")
+  spark.read.option("header",value = true).csv("data/website_pageviews.csv").createOrReplaceTempView("website_pageviews")
+  spark.read.option("header",value = true).csv("data/website_sessions.csv").createOrReplaceTempView("website_sessions")
+
+
+  spark.sql(
+    """
+      |SELECT * FROM orders
+      |""".stripMargin).show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  spark.close()
+
+}
